b9174cef64bbab21e6195418eb562f63d1b1d577
Pavel Pachkovskij <pavel.pachkovskij@gmail.com>
2012-06-07 11:57:50 +0300

    read me and initial sources

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
-sublime-html-to-haml
+# HTML to Haml plugin for Sublime Text 2
 ====================
 
-Converts files, selection and clipboard content from HTML or ERB to HAML using html2haml.heroku.com API
+Converts files, selection and clipboard content from HTML or ERB to HAML using http://html2haml.heroku.com API
+
+## Install
+**With Git:** Clone the repository in your Sublime Text "Packages" directory:
+
+    git clone git://github.com/pavelpachkovskij/sublime-html-to-haml.git
+
+
+The "Packages" directory is located at:
+
+* OS X:
+
+        ~/Library/Application Support/Sublime Text 2/Packages/
+
+* Linux:
+
+        ~/.config/sublime-text-2/Packages/
+
+* Windows:
+
+        %APPDATA%/Sublime Text 2/Packages/
+
+## Usage
+
+* **Convert hole ERB or HTML file** `Shift+Alt+F` - creates new file in the same folder using the same name as the source ending with .haml. Works only in .html and .erb files.
+* **Convert selection** `Shift+Alt+S` - replaces selection of HTML or ERB with HAML content. Works only in .haml files.
+* **Convert clipboard content** `Shift+Alt+S` - inserts HAML of converted clipboard HTML or ERB content. Works only in .haml files.
---
+++ html2haml.py
@@ -0,0 +1,60 @@
+import urllib2
+import json
+import sublime, sublime_plugin
+
+class HtmlToHamlFromFileCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		source = self.view.file_name()
+		if source.endswith(".erb"):
+			target = source.replace('.erb', '.haml')
+		if source.endswith(".html"):
+			target = source + '.haml'
+		if target:
+			with open(source, 'r') as f:
+				html = f.read()
+			haml = HTHTools.post_html_return_haml(html)
+			if haml != None:
+				with open(target, 'w') as f:
+					f.write(haml)
+				self.view.window().open_file(target)
+
+	def is_enabled(self):
+		return (self.view.file_name().endswith(".html") or self.view.file_name().endswith(".erb"))
+
+class HtmlToHamlFromSelectionCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		for region in self.view.sel():
+			if not region.empty():
+				html = self.view.substr(region)
+				haml = HTHTools.post_html_return_haml(html)
+				if haml != None:
+					self.view.replace(edit, region, haml)
+
+	def is_enabled(self):
+		return self.view.file_name().endswith(".haml")
+
+class HtmlToHamlFromClipboardCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		html = sublime.get_clipboard()
+		haml = HTHTools.post_html_return_haml(html)
+		if haml != None:
+			for region in self.view.sel():
+				self.view.replace(edit, region, haml)
+
+	def is_enabled(self):
+		return self.view.file_name().endswith(".haml")
+
+class HTHTools:
+	@classmethod
+	def post_html_return_haml(self, html):
+		host = 'http://html2haml.heroku.com/api.json'
+		data = {'page': {'html': html}}
+		data_json = json.dumps(data)
+		req = urllib2.Request(host, data_json, {'content-type': 'application/json'})
+		response_stream = urllib2.urlopen(req)
+		result = json.loads(response_stream.read())
+
+		if result["page"]:
+			return result["page"]["haml"]
+		else:
+			return None
